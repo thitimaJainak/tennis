f529c9f53e25a66c025bef959a965e157ebf7412
thitima Jainak <58660129@go.buu.ac.th>
2017-09-02 12:10:41 +0700

    echo fifteen - Love when playerA get first score

--- tennis.spec.js
+++ tennis.spec.js
@@ -1,19 +1,41 @@
-function TennisGame(){
-  this.reset = () => {
+function TennisGame() {
+    const scoreString = ['Love', 'Fifteen', 'Thirty', 'Forthy']
+    this.playerAScore = 0
 
-  }
-  this.echo = () => {
-    return 'Love - Love'
-  }
+    this.reset = () => {
+        this.playerAScore = 0
+    }
+
+    this.echo = () => {
+       return `${scoreString[this.playerAScore]} - Love`
+    }
+
+    this.playerAGteScore = () => {
+        this.playerAScore++
+    }
 }
 
-test('Echo "Love - Love " When game start',() => {
- //Arrangs
- let app = new TennisGame()
- //Act
- app.reset()
- let result = app.echo()
- //Assert
- expect(result).toBe('Love - Love')
+test('Echo "Love - Love" when game start', () => {
+    // Arrange
+    let app = new TennisGame()
 
+    // Act
+    app.reset()
+    let result = app.echo()
+
+    // Assert
+    expect(result).toBe('Love - Love')
+})
+
+test('Echo "Fifteen - Love" when playerA get first score', () => {
+    // Arrange
+    let app = new TennisGame()
+    app.reset()
+    app.playerAGteScore()
+
+    // Act
+    let result = app.echo()
+
+    // Assert
+    expect(result).toBe('Fifteen - Love')
 })
